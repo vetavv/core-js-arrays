968c2cb791c3bda5b5c4123788c091071f769b80
Violetta Vinakova <vinakova.vita@gmail.com>
2025-10-25 00:22:48 +0300

    feat: implement calculateBalance()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -324,8 +324,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr
+    .map(([income, expense]) => income - expense)
+    .reduce((sum, diff) => sum + diff, 0);
 }
 
 /**
