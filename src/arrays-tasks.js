bb760ff019994c4cec9bb6f7251571ed7f6044c9
Violetta Vinakova <vinakova.vita@gmail.com>
2025-10-27 23:26:04 +0300

    feat: implement getHexRGBValues()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -455,8 +455,11 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((decimal) => {
+    const hex = decimal.toString(16);
+    return `#${hex.toUpperCase().padStart(6, '0')}`;
+  });
 }
 
 /**
