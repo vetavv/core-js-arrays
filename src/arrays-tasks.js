a52c4ae1d7c3a844ae8dab67ba7070031ad6a3a3
Violetta Vinakova <vinakova.vita@gmail.com>
2025-10-27 23:21:37 +0300

    feat: implement getIndicesOfOddNumbers()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -441,8 +441,8 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.map((el, i) => (el % 2 !== 0 ? i : [])).flat();
 }
 
 /**
