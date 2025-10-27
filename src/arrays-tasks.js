d32689ec2b66aa16aae068cb2ddd12f974c567ca
Violetta Vinakova <vinakova.vita@gmail.com>
2025-10-27 23:32:58 +0300

    feat: implement findCommonElements()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -492,8 +492,13 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.reduce((acc, el) => {
+    if (arr2.includes(el)) {
+      return [...acc, el];
+    }
+    return acc;
+  }, []);
 }
 
 /**
